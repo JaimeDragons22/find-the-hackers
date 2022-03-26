5e6da300c9ab0c7b5d19668a4ad103a97da1b36d
JaimeDragons22 <101519803+JaimeDragons22@users.noreply.github.com>
2022-03-26 09:38:28 +0100

    Archivos iniciales del proyecto de MakeCode

---
+++ main.py
@@ -0,0 +1,33 @@
+def on_life_zero():
+    game.over(False)
+info.on_life_zero(on_life_zero)
+
+scene.set_background_image(assets.image("""
+    Map
+"""))
+mySprite = sprites.create(img("""
+        . . . . . . f f f f . . . . . . 
+            . . . . f f f 2 2 f f f . . . . 
+            . . . f f f 2 2 2 2 f f f . . . 
+            . . f f f e e e e e e f f f . . 
+            . . f f e 2 2 2 2 2 2 e e f . . 
+            . . f e 2 f f f f f f 2 e f . . 
+            . . f f f f e e e e f f f f . . 
+            . f f e f b f 4 4 f b f e f f . 
+            . f e e 4 1 f d d f 1 4 e e f . 
+            . . f e e d d d d d d e e f . . 
+            . . . f e e 4 4 4 4 e e f . . . 
+            . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+            . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+            . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+            . . . . . f f f f f f . . . . . 
+            . . . . . f f . . f f . . . . .
+    """),
+    SpriteKind.player)
+game.set_dialog_frame(assets.image("""
+    Frame
+"""))
+game.show_long_text("Find the Hackers", DialogLayout.BOTTOM)
+controller.move_sprite(mySprite, 100, 100)
+info.set_life(3)
+info.start_countdown(60)
